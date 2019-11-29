e0824aa920c8d6bcee5da276e6d50bc623cd275c
Abingcbc <19990626.love@163.com>
2019-11-29 16:37:28 +0800

    metadata

--- metadata-service/src/main/java/org/sse/metadataservice/controller/MetadataController.java
+++ metadata-service/src/main/java/org/sse/metadataservice/controller/MetadataController.java
@@ -1,4 +1,16 @@
 package org.sse.metadataservice.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author cbc
+ */
+@RestController
 public class MetadataController {
+
+    @GetMapping(value = "/test")
+    public int getTest() {
+        return 1;
+    }
 }
