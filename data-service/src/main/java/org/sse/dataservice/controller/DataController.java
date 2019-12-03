3483e054443b2c369a523a0b098acac6b90c13cc
Abingcbc <19990626.love@163.com>
2019-12-03 11:05:19 +0800

    dock test

--- data-service/src/main/java/org/sse/dataservice/controller/DataController.java
+++ data-service/src/main/java/org/sse/dataservice/controller/DataController.java
@@ -1,5 +1,6 @@
 package org.sse.dataservice.controller;
 
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
